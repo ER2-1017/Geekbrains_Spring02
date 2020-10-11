9df4054e7119bb4ea4ece6baceeb941cd283a989
ER2-1017 <60078003+ER2-1017@users.noreply.github.com>
2020-10-12 02:08:21 +0300

    Add files via upload

---
+++ src/test/java/GB/Abrosimov/demo/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package GB.Abrosimov.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
