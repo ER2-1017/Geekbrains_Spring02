ddba65e9eb9bf3b3e38076651bdd362e10b2febe
ER2-1017 <60078003+ER2-1017@users.noreply.github.com>
2020-10-15 06:12:05 +0300

    Homework 2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
+  <b-container id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Main</router-link> |
+      <router-link to="/catalog">Catalog</router-link>
     </div>
     <router-view/>
-  </div>
+  </b-container>
 </template>
 
 <style lang="scss">
---
+++ src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <Categories />
+  </div>
+</template>
+
+<script>
+
+import Categories from '@/components/Categories.vue';
+
+export default {
+  name: 'Main',
+  components: {
+    Categories,
+  },
+};
+</script>
